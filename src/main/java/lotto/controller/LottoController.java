4b1b6ad2387f4cf51d8ad0526631c9832a64f7c9
Chun Hyunwoo <1000chw@gmail.com>
2023-11-08 20:18:00 +0900

    init: LottoController init

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,7 +1,40 @@
 package lotto.controller;
 
+import lotto.model.Lotto;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class LottoController {
+    List<Lotto> lottos;
+    public LottoController() {
+        lottos = new ArrayList<>();
+    }
     public void lottoGamePlay() {
+        inputLottoEA();
+        printLottos();
+        inputAnswerLotto();
+        inputBonusNumber();
+        printStatistics();
+    }
+
+    private void inputLottoEA() {
+
+    }
+
+    private void printLottos() {
+
+    }
+
+    private void inputAnswerLotto(){
+
+    }
+
+    private void inputBonusNumber() {
+
+    }
+
+    private void printStatistics() {
 
     }
 }
