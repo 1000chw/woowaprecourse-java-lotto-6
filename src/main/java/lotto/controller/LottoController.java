30c5d9e0cb724fa057252fe5c16c9954feb2c8f1
Chun Hyunwoo <1000chw@gmail.com>
2023-11-08 20:32:33 +0900

    feat: InputView update inputPrice function update

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,21 +1,35 @@
 package lotto.controller;
 
 import lotto.model.Lotto;
+import lotto.view.InputView;
+import lotto.view.OutputView;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class LottoController {
-    List<Lotto> lottos;
+    private final InputView inputView;
+    private final OutputView outputView;
+    private List<Lotto> lottos;
+    private int price;
+
     public LottoController() {
         lottos = new ArrayList<>();
+        inputView = new InputView();
+        outputView = new OutputView();
     }
     public void lottoGamePlay() {
+        inputPrice();
         inputLottoEA();
         printLottos();
         inputAnswerLotto();
         inputBonusNumber();
         printStatistics();
+    }
+
+    private void inputPrice() {
+        outputView.printPriceInputInform();
+        price = inputView.inputPrice();
     }
 
     private void inputLottoEA() {
--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,4 +1,13 @@
 package lotto.view;
 
 public class InputView {
+    public int inputPrice() {
+        String price = camp.nextstep.edu.missionutils.Console.readLine();
+        try {
+            int priceTotal = Integer.parseInt(price);
+            return priceTotal;
+        }catch (IllegalArgumentException e) {
+            return inputPrice();
+        }
+    }
 }
